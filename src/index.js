c1cc2b23c4d7608a2347e2e7a542c31621ed805a
Diego Stiehl <seccomiro@gmail.com>
2020-04-22 10:10:51 -0300

    Move store to a new file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
-import reducers from './reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
-);
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
